e2eaa5df8fef09d4dce26baad7b38d85d668889b
José A. Pilartes-Congo Jr <jcongo@islander.tamucc.edu>
2024-02-28 10:58:30 -0600

    Update myFunction.py

--- myFunction.py
+++ myFunction.py
@@ -13,36 +13,6 @@ import scipy.stats as stats
 from scipy.stats import pearsonr
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
-# ------------------------------------------
-# function to plot general reference map of focus areas
-def general_reference_map(coords):
-  
-  # set WGS84 as coordinate system
-  coords_crs = 'EPSG:4326'
-  projection = ccrs.LambertConformal()
-  
-  # display general reference (gr) map
-  fig, ax = plt.subplots(figsize=(10, 10), subplot_kw={'projection': ccrs.PlateCarree()})
-  
-  # plot coordinates on the map and add annotations for each location
-  ax.scatter(x=[coord[1] for coord in coords], y=[coord[0] for coord in coords], transform=ccrs.PlateCarree())
-  for i, coord in enumerate(coords):
-    ax.text(coord[1], coord[0], coord[2], transform=ccrs.PlateCarree())
-  
-  # add gridlines and coastline boundaries
-  ax.gridlines(draw_labels=True, color='none')
-  ax.coastlines()
-  
-  # set map extent to area of interest
-  states = cfeature.NaturalEarthFeature("cultural", "admin_1_states_provinces", "10m", facecolor="none", edgecolor="silver")
-  ax.add_feature(states)
-  ax.set_extent([-0.8e6, 0.7e6, -1.45e6, -0.7e6], crs=projection)
-  
-  # Set the title and show the plot
-  ax.set_title('Coordinates of Locations of Interest')
-  plt.show()
-
-
 # --------------------------------------------------------------------------
 # function to group features according to location
 def indiv_station(dataset, city_name):
