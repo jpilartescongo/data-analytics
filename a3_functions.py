0a0559f3f1fedba6269bf2c0b532a2495204db09
José A. Pilartes-Congo Jr <jcongo@islander.tamucc.edu>
2024-03-27 16:57:34 -0500

    Update a3_functions.py

--- a3_functions.py
+++ a3_functions.py
@@ -5,7 +5,7 @@ import os, pandas as pd
 
 # ----------------------------------------------
 # get the count of nans for various variables
-def count_nan_values(df, vars):
+def get_count_of_nans(df, vars):
   nan_counts = df[vars].isna().sum()
   nan_counts_df = pd.DataFrame(nan_counts, columns=['Count'])
   return nan_counts_df
