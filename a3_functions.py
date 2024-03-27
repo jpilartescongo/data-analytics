89dfd6c8b1dcae232087676ee5d32ce9754cba89
José A. Pilartes-Congo Jr <jcongo@islander.tamucc.edu>
2024-03-27 16:39:38 -0500

    Update a3_functions.py

--- a3_functions.py
+++ a3_functions.py
@@ -4,4 +4,8 @@
 import os, pandas as pd
 
 # ----------------------------------------------
-# merge multiple csv files into a single one
+# get the count of nans for various variables
+def get_count_of_nans(dataset, variables):
+  nan_count = dataset[variables].isna().sum()
+  nan_count_df = pd.DataFrame(nan_count, columns=['Count:'])
+  return nan_count_df
