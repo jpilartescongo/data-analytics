be44e495ace91d38436de935cd007005837c7ffa
José A. Pilartes-Congo Jr <jcongo@islander.tamucc.edu>
2024-03-27 22:54:48 -0500

    Update a3_functions.py

--- a3_functions.py
+++ a3_functions.py
@@ -18,22 +18,26 @@ def fill_nans(dataframe):
     date = row['date']
     wind_speed_nan = pd.isna(row['wind_speed'])
     wind_gust_nan = pd.isna(row['wind_gust'])
-    
-    # check if there are missing values for wind_speed or wind_gust
-    if wind_speed_nan or wind_gust_nan:
+    water_temp_nan = pd.isna(row['water_temp'])
+
+    # check for are missing wind_speed, wind_gust, or water_temp values
+    if wind_speed_nan or wind_gust_nan or water_temp_nan:
       # Filter the corresponding records for the specific date
       same_day_records = dataframe[dataframe['date'] == date]
-      
-      # calculate the average for 'wind_speed' and 'wind_gust' on that day
+
+      # calculate the average for 'wind_speed', 'wind_gust', and 'water_temp' on that day
       avg_wind_speed = same_day_records['wind_speed'].mean()
       avg_wind_gust = same_day_records['wind_gust'].mean()
-      
+      avg_water_temp = same_day_records['water_temp'].mean()
+
       # replace NaN values with the calculated averages
       if wind_speed_nan:
         dataframe.at[index, 'wind_speed'] = avg_wind_speed
       if wind_gust_nan:
         dataframe.at[index, 'wind_gust'] = avg_wind_gust
-    
-  # Reset index after modification
+      if water_temp_nan:
+        dataframe.at[index, 'water_temp'] = avg_water_temp
+
+  # reset index after modification
   dataframe.reset_index(drop=True, inplace=True)
   return dataframe
